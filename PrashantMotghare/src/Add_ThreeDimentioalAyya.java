98f953e2ba8dd1fde8d6f2727271877f760798fc
apmosys_2 <apmosys_2@APMOSYSLT0129>
2022-11-19 10:11:00 +0530

    gi

---
+++ PrashantMotghare/src/Add_ThreeDimentioalAyya.java
@@ -0,0 +1,21 @@
+
+public class Add_ThreeDimentioalAyya {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           int a[][][]= {{{1,2,3},{4,5,6},{7,8,9}}};
+           int b[][][]= {{{9,8,7},{4,5,6},{3,2,1}}};
+           int c[][][]=new int [3][3][3];
+           for(int i=0;i<3;i++) {
+        	   for(int j=0;j<3;j++) {
+        		   for(int k=0;k<3;k++) {
+        			   c[i][j][k]=a[i][j][k]+b[i][j][k];
+        			   System.out.println(c[i][j][k]);
+        		   }
+        		   System.out.print(" ------- ");
+        	   }
+        	   System.out.print("======= ");
+           }
+	}
+
+}
