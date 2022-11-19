cff91421e6c9762ffd1370c493b9c4f33d18b929
apmosys_2 <apmosys_2@APMOSYSLT0129>
2022-11-19 10:06:55 +0530

    by vchfh

---
+++ PrashantMotghare/src/demo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a[]= {45, 12, 55, 78, 445};
+int sum=0;
+
+for(int i=0;i<a.length;i++)
+{
+	sum=sum+a[i];
+}
+System.out.println(sum);
+
+	}
+
+}
