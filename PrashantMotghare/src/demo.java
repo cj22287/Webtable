86fc6892225c295f2dc62b98af50fc3e7cec4e58
cj22287 <118585302+cj22287@users.noreply.github.com>
2022-11-19 10:16:16 +0530

    donw
    k

--- PrashantMotghare/src/demo.java
+++ PrashantMotghare/src/demo.java
@@ -4,7 +4,7 @@ public class demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int a[]= {45, 12, 55, 78, 445};
+int a[]= {45, 12, 55, 78, 445, 888};
 int sum=0;
 
 for(int i=0;i<a.length;i++)
